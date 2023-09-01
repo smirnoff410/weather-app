a4fe5eff0ee906437929db859f70234b6630b3e4
vladislav.smirnov <vladislav.smirnov@streamlabs.ru>
2023-09-01 10:17:40 +0300

    init app

---
+++ WeatherDesktop/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WeatherDesktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ShowWeather_Click(object sender, RoutedEventArgs e)
+        {
+            var httpClient = new HttpClient();
+            var result = await httpClient.GetFromJsonAsync<ICollection<WeatherDTO>>("http://localhost:5000/weatherforecast");
+            if(result == null)
+            {
+                MessageBox.Show("Данные о погоде пусты");
+                return;
+            }
+            WeatherList.Items.Clear();
+            foreach (var weather in result)
+                WeatherList.Items.Add(weather);
+        }
+    }
+}
---
+++ WeatherDesktop/WeatherDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WeatherDesktop
+{
+    public class WeatherDTO
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF { get; set; }
+
+        public string? Summary { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Date}. TemperatureC: {TemperatureC}. TemperatureF: {TemperatureF}. Summary: {Summary}";
+        }
+    }
+}
